35877999434f8995088cd53abeecd0abd4b12adf
chappertron <52535659+chappertron@users.noreply.github.com>
2023-11-17 14:22:53 +0000

    Minor tidy up

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,29 @@ use io::Write;
 use std::io;
 
 fn main() -> Result<()> {
-    let ProductIndex { index, dimensions } = ProductIndex::parse();
+    let ProductIndex { index, lengths } = ProductIndex::parse();
 
-    let indicies = product_index_Nd(index, &dimensions)?;
+    let indicies = product_index_Nd(index, &lengths)?;
 
+    // Lock to prevent repeated locking.
     let mut std_out = io::stdout().lock();
 
     for i in indicies {
         // TODO: Remove trailing whitespace?
         write!(std_out, "{i} ")?;
     }
+
     writeln!(std_out,)?;
 
     Ok(())
 }
 
 #[derive(Parser, Debug)]
+/// A command line tool to get the indicies to that correspond to a Cartesian product.
 struct ProductIndex {
+    /// 1D Index to convert to N-Dimensional.
     index: usize,
-    dimensions: Vec<usize>,
+    /// Length of each of the N arrays.
+    #[arg(required = true)]
+    lengths: Vec<usize>,
 }
